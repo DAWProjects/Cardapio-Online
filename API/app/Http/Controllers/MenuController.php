4213d7401ce1badd4b8484ee369431e3e23a6c23
Elton Tembe <eltontembe10@gmail.com>
2016-09-20 10:35:45 +0200

    Models And Controllers Commit

---
+++ API/app/Http/Controllers/MenuController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Menu;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $refeicoes = Menu::all();
+        return response()->json($refeicoes->toArray());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $refeicao = Menu::create($request->all());
+        $refeicao->save();
+        return response()->json(["mensagem"=>"Refeicao Registada Com Sucesso!"]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $refeicao = Menu::find($id);
+        return response()->json($refeicao->toArray());
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $refeicao = Menu::find($id);
+        $refeicao->fill($request->all());
+        $refeicao->save();
+        return response()->json(["mensagem"=>"Refeicao Actualizada Com Sucesso"]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Menu::destroy($id);
+
+    }
+}
